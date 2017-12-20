5ced2ed0cf7c8a3a394f9eaa39bb006a1e374ef2
Chinmay chandragiri <chinmay.chandragiri@personalcapital.com>
2017-12-20 14:55:25 -0800

    toggle status

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response, Headers} from '@angular/http';
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-user',
@@ -10,19 +12,28 @@ export class UserComponent implements OnInit {
 
   constructor(private http: Http) { }
   
-  users = [];
-  fetchUsers = function() {
-     this.http.get("https://pcap-amd.herokuapp.com/users").subscribe(
+  usersInQueue = [];
+  fetchUsersInQueue = function() {
+     this.http.get("https://pcap-amd.herokuapp.com/users/userQueue").subscribe(
      	(res: Response) => {
-     	    this.users = res.json();
+     	    this.usersInQueue = res.json();
      	}
      )
   };
 
+  users = [];
+  fetchUsers = function() {
+     this.http.get("https://pcap-amd.herokuapp.com/users").subscribe(
+      (res: Response) => {
+          this.users = res.json();
+      }
+     )
+  };
+
   associate = {
-  firstName:"",
-  lastName:"",
-  status:""
+    firstName:"",
+    lastName:"",
+    status:""
   };
   fetchAssociate = function() {
      this.http.get("https://pcap-amd.herokuapp.com/salesAssociate/9970011801").subscribe(
@@ -32,9 +43,48 @@ export class UserComponent implements OnInit {
      )
   };
 
+  toggleStatus = {
+    status:'Offline'
+  };
+
+  getToggleStatus = function() {
+     this.http.ger("https://pcap-amd.herokuapp.com/salesAssociate/9970011801/toggle").subscribe(
+      (res: Response) => {
+          this.toggleStatus = res.json();
+      }
+     )
+  };
+
+  updateToggleStatus = function() {
+     this.http.put("https://pcap-amd.herokuapp.com/salesAssociate/9970011801/toggle").subscribe(
+      (res: Response) => {
+          this.toggleStatus = res.json();
+      }
+     )
+  };
+
   ngOnInit() {
+      this.fetchUsersInQueue();
       this.fetchUsers();
       this.fetchAssociate();
+      this.getToggleStatus();
+
+      var userComponent = this;
+      
+      $('#toggle_event_editing button').click(function(){
+      
+      if($(this).hasClass('locked_active') || $(this).hasClass('unlocked_inactive')){
+            userComponent.updateToggleStatus();
+      } else{
+            userComponent.updateToggleStatus();
+      }
+      
+      /* reverse locking status */
+      $('#toggle_event_editing button').eq(0).toggleClass('locked_inactive locked_active btn-default btn-info');
+      $('#toggle_event_editing button').eq(1).toggleClass('unlocked_inactive unlocked_active btn-info btn-default');
+    });
+
+
   }
 
 }
